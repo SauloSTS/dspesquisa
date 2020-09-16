10e8e341a8942499ec8e60b15500421bc53c0b5d
Saulo Tavares Silva <sts_10@hotmail.com>
2020-09-16 12:03:37 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/vspesquisa/VspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VspesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/vspesquisa/VspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
